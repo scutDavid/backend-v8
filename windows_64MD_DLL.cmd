d7c9ffd4ef5b69d1f217cf35d94a8a4378ee41c6
scutDavid <1902828943@qq.com>
2024-02-06 15:13:12 +0800

    测试提交txt

--- windows_64MD_DLL.cmd
+++ windows_64MD_DLL.cmd
@@ -1,12 +1,6 @@
 set VERSION=%1
-%HOMEDRIVE%
-cd %HOMEDRIVE%%HOMEPATH%
 
-mkdir v8
-cd v8
-mkdir v8
-cd v8
-md output\v8\Lib\Win64DLL
+md $GITHUB_WORKSPACE/output\v8\Lib\Win64DLL
 cd output\v8\Lib\Win64DLL
 cd > filename.txt
 echo =====[ finish ]=====
