de96316d180559ba7581854b2e7275e735091e5e
scutDavid <1902828943@qq.com>
2024-02-06 15:23:03 +0800

    测试提交txt

--- windows_64MD_DLL.cmd
+++ windows_64MD_DLL.cmd
@@ -1,6 +1,6 @@
 set VERSION=%1
 
-md $GITHUB_WORKSPACE\output\v8\Lib\Win64DLL
-cd $GITHUB_WORKSPACE\output\v8\Lib\Win64DLL
+md $GITHUB_WORKSPACE\v8\v8\output\v8\Lib\Win64DLL
+cd $GITHUB_WORKSPACE\v8\v8\output\v8\Lib\Win64DLL
 cd > filename.txt
 echo =====[ finish ]=====
