19fcecec5b44cbc4d358cce27b4bbb7c4e56b082
scutDavid <1902828943@qq.com>
2024-02-20 11:58:44 +0800

    测试提交txt

--- windows_64MD_DLL.cmd
+++ windows_64MD_DLL.cmd
@@ -3,4 +3,9 @@ set VERSION=%1
 md $GITHUB_WORKSPACE\v8\v8\output\v8\Lib\Win64DLL
 cd $GITHUB_WORKSPACE\v8\v8\output\v8\Lib\Win64DLL
 cd > filename.txt
+echo > filename2.txt
+dir /b > test.txt
+echo %cd%
+dir
+more +0 test.txt
 echo =====[ finish ]=====
