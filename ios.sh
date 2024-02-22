85194979c4b79a00b995140bf8ccda90871ff6a1
scutDavid <1902828943@qq.com>
2024-02-22 14:37:23 +0800

    测试iospython路径

--- ios.sh
+++ ios.sh
@@ -21,47 +21,48 @@ cd ..
 export DEPOT_TOOLS_UPDATE=0
 export PATH=$(pwd)/depot_tools:$PATH
 export PATH=$(pwd)/depot_tools/.cipd_bin/2.7/bin:$PATH
+python --version
 gclient
 
-mkdir v8
-cd v8
+# mkdir v8
+# cd v8
 
-echo "=====[ Fetching V8 ]====="
-fetch v8
-echo "target_os = ['ios']" >> .gclient
-cd ~/v8/v8
-git checkout cfr_v8_8.4-lkgr
-gclient sync
+# echo "=====[ Fetching V8 ]====="
+# fetch v8
+# echo "target_os = ['ios']" >> .gclient
+# cd ~/v8/v8
+# git checkout cfr_v8_8.4-lkgr
+# gclient sync
 
-# echo "=====[ Patching V8 ]====="
-# git apply --cached $GITHUB_WORKSPACE/patches/builtins-puerts.patches
-# git checkout -- .
+# # echo "=====[ Patching V8 ]====="
+# # git apply --cached $GITHUB_WORKSPACE/patches/builtins-puerts.patches
+# # git checkout -- .
 
-# echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
-# node $GITHUB_WORKSPACE/node-script/add_arraybuffer_new_without_stl.js .
+# # echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
+# # node $GITHUB_WORKSPACE/node-script/add_arraybuffer_new_without_stl.js .
 
-echo "=====[ Building V8 ]====="
-python ./tools/dev/v8gen.py arm64.release -vv -- '
-v8_use_external_startup_data = true
-v8_use_snapshot = true
-v8_enable_i18n_support = true
-is_debug = false
-v8_static_library = true
-ios_enable_code_signing = false
-target_os = "ios"
-target_cpu = "arm64"
-v8_enable_pointer_compression = false
-libcxx_abi_unstable = false
-'
-ninja -C out.gn/arm64.release -t clean
-ninja -C out.gn/arm64.release wee8
-strip -S out.gn/arm64.release/obj/libwee8.a
+# echo "=====[ Building V8 ]====="
+# python ./tools/dev/v8gen.py arm64.release -vv -- '
+# v8_use_external_startup_data = true
+# v8_use_snapshot = true
+# v8_enable_i18n_support = true
+# is_debug = false
+# v8_static_library = true
+# ios_enable_code_signing = false
+# target_os = "ios"
+# target_cpu = "arm64"
+# v8_enable_pointer_compression = false
+# libcxx_abi_unstable = false
+# '
+# ninja -C out.gn/arm64.release -t clean
+# ninja -C out.gn/arm64.release wee8
+# strip -S out.gn/arm64.release/obj/libwee8.a
 
-node $GITHUB_WORKSPACE/node-script/genBlobHeader.js "ios arm64" out.gn/arm64.release/snapshot_blob.bin
+# node $GITHUB_WORKSPACE/node-script/genBlobHeader.js "ios arm64" out.gn/arm64.release/snapshot_blob.bin
 
-mkdir -p output/v8/Lib/iOS/arm64
-cp out.gn/arm64.release/obj/libwee8.a output/v8/Lib/iOS/arm64/
-cp out.gn/arm64.release/icudtl.dat output/v8/Lib/iOS/arm64/
+# mkdir -p output/v8/Lib/iOS/arm64
+# cp out.gn/arm64.release/obj/libwee8.a output/v8/Lib/iOS/arm64/
+# cp out.gn/arm64.release/icudtl.dat output/v8/Lib/iOS/arm64/
 
-mkdir -p output/v8/Inc/Blob/iOS/arm64
-cp SnapshotBlob.h output/v8/Inc/Blob/iOS/arm64/
+# mkdir -p output/v8/Inc/Blob/iOS/arm64
+# cp SnapshotBlob.h output/v8/Inc/Blob/iOS/arm64/
