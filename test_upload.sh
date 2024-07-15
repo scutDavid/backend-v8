cf314ba30072c21fff60798e6137f51c95bcd6ed
scutDavid <1902828943@qq.com>
2024-07-15 10:38:59 +0800

    测试上传

--- test_upload.sh
+++ test_upload.sh
@@ -1,4 +1,4 @@
-[ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
+[ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )" && pwd )"
 echo $GITHUB_WORKSPACE
 mkdir -p $GITHUB_WORKSPACE/output/v8/Lib/Test
 cd $GITHUB_WORKSPACE/output/v8/Lib/Test 
