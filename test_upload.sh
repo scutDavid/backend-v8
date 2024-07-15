ffcc790e5e9d98775ab6207129bb33b9a277beed
scutDavid <1902828943@qq.com>
2024-07-15 10:22:21 +0800

    测试上传

--- test_upload.sh
+++ test_upload.sh
@@ -1,3 +1,4 @@
 [ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
+echo $GITHUB_WORKSPACE
 mkdir -p $GITHUB_WORKSPACE/output/v8/Lib/Test
 echo "This is a test file" > test.txt
